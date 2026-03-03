9935466f39a1ffc1c3573327f12997f6581ef76d
synkupcode <synkupcode@gmail.com>
2026-03-03 14:49:15 +0530

    Update email_service.py

--- app/services/email_service.py
+++ app/services/email_service.py
@@ -5,24 +5,19 @@ from email.mime.multipart import MIMEMultipart
 from app.core.config import settings
 
 
+dfrom app.integrations.erp_client import erp_request
+
+
 def send_email(to_email: str, subject: str, html_content: str):
 
-    if not settings.SMTP_HOST:
-        raise Exception("SMTP not configured")
-
-    msg = MIMEMultipart()
-    msg["From"] = settings.SMTP_FROM_EMAIL
-    msg["To"] = to_email
-    msg["Subject"] = subject
-
-    msg.attach(MIMEText(html_content, "html"))
-
-    server = smtplib.SMTP(settings.SMTP_HOST, settings.SMTP_PORT)
-
-    if settings.SMTP_USE_TLS:
-        server.starttls()
-
-    server.login(settings.SMTP_USERNAME, settings.SMTP_PASSWORD)
-
-    server.send_message(msg)
-    server.quit()
+    erp_request(
+        method="POST",
+        path="/api/method/frappe.core.doctype.communication.email.make",
+        json={
+            "recipients": to_email,
+            "subject": subject,
+            "content": html_content,
+            "communication_medium": "Email",
+            "send_email": 1
+        }
+    )
